85de26443e04a704d9983dc7b67bd2da0c7cfb5f
do00ob <1955788420@qq.com>
2023-09-10 23:17:58 +0800

    123

--- src/main/java/com/example/demo/common/QueryPageParam.java
+++ src/main/java/com/example/demo/common/QueryPageParam.java
@@ -1,4 +1,13 @@
 package com.example.demo.common;
 
+import lombok.Data;
+
+@Data
 public class QueryPageParam {
+    //默认
+    private static int PAGE_SIZE=20;
+    private static int PAGE_NUM=1;
+
+    private int pageSize;
+    private int pageNum;
 }
